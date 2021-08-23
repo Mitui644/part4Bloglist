e413de2372d8e3c34c9d9e14d84c81ed18c6e6f4
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-23 16:04:14 +0300

    Solution for 4_5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,16 +1,32 @@
 const dummy = (blogs) => {
     return 1
   }
-  
-  module.exports = {
-    dummy
-  }
 
   const totalLikes = (blogs) => {
     return blogs.reduce((acc, item) => acc + item.likes, 0)
   }
+
+  const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) {
+        return {}
+    }
+
+    var maxBlog = blogs[0]
+    for(const curr of blogs) {
+        if (curr.likes > maxBlog.likes) {
+            maxBlog = curr
+        }
+    }
+    
+    return {
+        title: maxBlog.title,
+        author: maxBlog.author,
+        likes: maxBlog.likes
+    }
+  }
   
   module.exports = {
     dummy, 
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
