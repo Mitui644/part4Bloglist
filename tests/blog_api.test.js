8e2404e1ea9a010fca5e5441632fe48099680506
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-24 12:07:36 +0300

    Solution for 4_9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,7 +12,7 @@ beforeEach(async () => {
     for(const blog of helper.initialBlogs) {
         await new Blog(blog).save()
     }
-})
+}, 100000)
 
 test('correct amount of blogs are returned as json', async () => {
   const response = await api.get('/api/blogs')
@@ -20,6 +20,14 @@ test('correct amount of blogs are returned as json', async () => {
     expect(response.headers['content-type']).toMatch(/application\/json/)
     expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
+
+test('unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    for(const blog of response.body) {
+        expect(blog.id).toBeDefined()
+        expect(blog._id).toBeFalsy()
+    }
+  })
 
 afterAll(() => {
   mongoose.connection.close()
