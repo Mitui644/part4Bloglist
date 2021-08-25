ff5117d467cd1e0b40a5373cd94fe2eecb303ad8
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-25 12:06:37 +0300

    Solution for 4_20

--- utils/middleware.js
+++ utils/middleware.js
@@ -22,6 +22,17 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      request.token = authorization.substring(7)
+    } else {
+      request.token = null
+    }
+    next()
+}
+
 module.exports = {
-  requestLogger, errorHandler
+  requestLogger, errorHandler, tokenExtractor
 }
